d8ba22d23fa834ed4f9dae5e1e11d498eecbe363
kunleadeniyi <adeniyikunle22@gmail.com>
2020-10-05 15:46:41 +0100

    another commit

--- config/authenticate.js
+++ config/authenticate.js
@@ -2,18 +2,22 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 async function ensureAuth(req, res, next) {
-    if (!req.header('Authorization')) {
-        return res.status(403).send("Forbidden")
+    try {
+        if (!req.header('Authorization')) {
+            return res.status(403).send("Forbidden")
+        }
+    
+        const token = req.header('Authorization').split(' ')[1];
+    
+        if (!token) {
+            return res.status(403).send("Forbidden")
+        }
+        req.token = token
+        req.user = await User.findByToken(token)
+        next()
+    } catch (error) {
+        next(error)
     }
-
-    const token = req.header('Authorization').split(' ')[1];
-
-    if (!token) {
-        return res.status(403).send("Forbidden")
-    }
-    req.token = token
-    req.user = await User.findByToken(token)
-    next()
 }
 
 module.exports = {
